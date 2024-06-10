5319d597b5dbbdf844ed1ababce09d5fa798acc9
Thees K <sandy.celeron@t-online.de>
2024-06-10 17:56:17 +0200

    Add track_formatter.py

--- display_playlist.py
+++ display_playlist.py
@@ -1,36 +1,6 @@
 import sqlite3
 import os
-
-
-def print_track_info(track):
-    (track_id, timestamp, title, movement, composer, full_title, image_link,
-     catalog_number, conductor, orchestra, solist, album, ensemble, ean, choir) = track
-
-    title = format_field(title)
-    movement = format_field(movement)
-    composer = format_field(composer)
-    catalog_number = format_field(catalog_number)
-    conductor = format_field(conductor)
-    orchestra = format_field(orchestra)
-    solist = format_field(solist)
-    album = format_field(album)
-    ensemble = format_field(ensemble)
-    ean = format_field(ean)
-    choir = format_field(choir)
-
-    composer_info = f"{composer}:" if composer else ""
-    piece_and_movement = ", ".join(filter(None, [title, movement]))
-    performers = ", ".join(filter(None, [solist, choir, ensemble, orchestra, conductor]))
-    performers = f"({performers})" if performers else ""
-    album_info = "/".join(filter(None, [album, ean, catalog_number]))
-    album_info = f"[{album_info}]" if album_info else ""
-    track_info = " ".join(filter(None, [composer_info, piece_and_movement, performers, album_info]))
-
-    print(f"{timestamp}: {track_info}")
-
-
-def format_field(field):
-    return field if field and field != '-' else ""
+from track_formatter import print_track_info
 
 
 def display_all_tracks(conn):
