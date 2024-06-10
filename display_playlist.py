36a0de51acbfeb5213e0b7522ed13d267cb37ee3
Thees K <sandy.celeron@t-online.de>
2024-06-10 17:41:56 +0200

    display_playlist.py shorter output

--- display_playlist.py
+++ display_playlist.py
@@ -2,31 +2,57 @@ import sqlite3
 import os
 
 
+def print_row(row):
+    (
+        id, timestamp, title, movement, composer, full_title, image_link,
+        catalog_number, conductor, orchestra, solist, album, ensemble, ean, choir
+    ) = row
+
+    title = format(title)
+    movement = format(movement)
+    composer = format(composer)
+    catalog_number = format(catalog_number)
+    conductor = format(conductor)
+    orchestra = format(orchestra)
+    solist = format(solist)
+    album = format(album)
+    ensemble = format(ensemble)
+    ean = format(ean)
+    choir = format(choir)
+
+    composer = "" if composer == "" else composer+ ":"
+    piece_and_movement = ", ".join(format_list([title, movement]))
+    performers = ", ".join(format_list([solist, choir, ensemble, orchestra, conductor]))
+    performers = performers if performers == "" else "("+performers+")"
+    album_info = "/".join(format_list([album, ean, catalog_number]))
+    album_info = album_info if album_info == "" else "[" + album_info + "]"
+    track_info = " ".join(format_list([composer, piece_and_movement, performers, album_info]))
+    print(f"{timestamp}: {track_info}")
+
+
+def format(str):
+    if not str or str == '-':
+        return ""
+    return str
+
+
+def format_list(list):
+    result = []
+    for s in list:
+        if s:
+            result.append(s)
+    return result
+
+
 def display_all_tracks(conn):
     cursor = conn.execute('SELECT * FROM Tracks ORDER BY timestamp ASC')
     results = cursor.fetchall()
-
     for row in results:
         (
             id, timestamp, title, movement, composer, full_title, image_link,
             catalog_number, conductor, orchestra, solist, album, ensemble, ean, choir
         ) = row
-        print(f"ID: {id}")
-        print(f"Timestamp: {timestamp}")
-        print(f"Title: {title}")
-        print(f"Movement: {movement}")
-        print(f"Composer: {composer}")
-        print(f"Full Title: {full_title}")
-        print(f"Image Link: {image_link}")
-        print(f"Catalog Number: {catalog_number}")
-        print(f"Conductor: {conductor}")
-        print(f"Orchestra: {orchestra}")
-        print(f"Solist: {solist}")
-        print(f"Album: {album}")
-        print(f"Ensemble: {ensemble}")
-        print(f"EAN: {ean}")
-        print(f"Choir: {choir}")
-        print("-" * 20)
+        print_row(row)
 
 
 def main():
