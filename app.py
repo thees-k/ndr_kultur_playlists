c828d36cfbf1a969ba45375230a73bd0e52f5191
Thees K <sandy.celeron@t-online.de>
2024-06-13 16:40:10 +0200

    Add special analyzis

--- app.py
+++ app.py
@@ -36,6 +36,62 @@ def index():
     return render_template('index.html', tracks=tracks)
 
 
+@app.route('/top_tracks')
+def top_tracks():
+    tracks = Tracks.query.all()
+    sorted_tracks = sorted(tracks, key=lambda x: (x.timestamp, x.title))
+    return render_template('analysis.html', title="Top 10 Tracks", tracks=sorted_tracks[:10])
+
+
+@app.route('/top_composers')
+def top_composers():
+    composers = db.session.query(Tracks.composer, db.func.count(Tracks.id)).group_by(Tracks.composer).order_by(
+        db.func.count(Tracks.id).desc()).limit(10).all()
+    return render_template('analysis.html', title="Top 10 Composers", items=composers)
+
+
+@app.route('/top_orchestras')
+def top_orchestras():
+    orchestras = db.session.query(Tracks.orchestra, db.func.count(Tracks.id)).group_by(Tracks.orchestra).order_by(
+        db.func.count(Tracks.id).desc()).limit(10).all()
+    return render_template('analysis.html', title="Top 10 Orchestras", items=orchestras)
+
+
+@app.route('/top_conductors')
+def top_conductors():
+    conductors = db.session.query(Tracks.conductor, db.func.count(Tracks.id)).group_by(Tracks.conductor).order_by(
+        db.func.count(Tracks.id).desc()).limit(10).all()
+    return render_template('analysis.html', title="Top 10 Conductors", items=conductors)
+
+
+@app.route('/top_albums')
+def top_albums():
+    albums = db.session.query(Tracks.album, db.func.count(Tracks.id)).group_by(Tracks.album).order_by(
+        db.func.count(Tracks.id).desc()).limit(10).all()
+    return render_template('analysis.html', title="Top 10 Albums", items=albums)
+
+
+@app.route('/top_solists')
+def top_solists():
+    solists = db.session.query(Tracks.solist, db.func.count(Tracks.id)).group_by(Tracks.solist).order_by(
+        db.func.count(Tracks.id).desc()).limit(10).all()
+    return render_template('analysis.html', title="Top 10 Solists", items=solists)
+
+
+@app.route('/top_catalog_numbers')
+def top_catalog_numbers():
+    catalog_numbers = db.session.query(Tracks.catalog_number, db.func.count(Tracks.id)).group_by(
+        Tracks.catalog_number).order_by(db.func.count(Tracks.id).desc()).limit(10).all()
+    return render_template('analysis.html', title="Top 10 Catalog Numbers", items=catalog_numbers)
+
+
+@app.route('/top_eans')
+def top_eans():
+    eans = db.session.query(Tracks.ean, db.func.count(Tracks.id)).group_by(Tracks.ean).order_by(
+        db.func.count(Tracks.id).desc()).limit(10).all()
+    return render_template('analysis.html', title="Top 10 EANs", items=eans)
+
+
 if __name__ == '__main__':
     # Sicherstellen, dass das .data-Verzeichnis existiert
     os.makedirs(os.path.join(os.path.dirname(__file__), '.data'), exist_ok=True)
