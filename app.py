a53be47498e7a113aa311081c6da65e5772f3554
Thees K <sandy.celeron@t-online.de>
2024-06-17 16:56:54 +0200

    Änderungen von app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import os
 
@@ -11,86 +11,49 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-
 class Tracks(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    timestamp = db.Column(db.String)
-    title = db.Column(db.String)
-    movement = db.Column(db.String)
-    composer = db.Column(db.String)
-    full_title = db.Column(db.String)
-    image_link = db.Column(db.String)
-    catalog_number = db.Column(db.String)
-    conductor = db.Column(db.String)
-    orchestra = db.Column(db.String)
-    solist = db.Column(db.String)
-    album = db.Column(db.String)
-    ensemble = db.Column(db.String)
-    ean = db.Column(db.String)
-    choir = db.Column(db.String)
-
+    timestamp = db.Column(db.String, unique=True)
+    title = db.Column(db.String, nullable=True)
+    movement = db.Column(db.String, nullable=True)
+    composer = db.Column(db.String, nullable=True)
+    full_title = db.Column(db.String, nullable=True)
+    image_link = db.Column(db.String, nullable=True)
+    catalog_number = db.Column(db.String, nullable=True)
+    conductor = db.Column(db.String, nullable=True)
+    orchestra = db.Column(db.String, nullable=True)
+    solist = db.Column(db.String, nullable=True)
+    album = db.Column(db.String, nullable=True)
+    ensemble = db.Column(db.String, nullable=True)
+    ean = db.Column(db.String, nullable=True)
+    choir = db.Column(db.String, nullable=True)
 
 @app.route('/')
 def index():
     tracks = Tracks.query.all()
     return render_template('index.html', tracks=tracks)
 
-
-@app.route('/top_tracks')
-def top_tracks():
+@app.route('/data')
+def data():
     tracks = Tracks.query.all()
-    sorted_tracks = sorted(tracks, key=lambda x: (x.timestamp, x.title))
-    return render_template('analysis.html', title="Top 10 Tracks", tracks=sorted_tracks[:10])
-
-
-@app.route('/top_composers')
-def top_composers():
-    composers = db.session.query(Tracks.composer, db.func.count(Tracks.id)).group_by(Tracks.composer).order_by(
-        db.func.count(Tracks.id).desc()).limit(10).all()
-    return render_template('analysis.html', title="Top 10 Composers", items=composers)
-
-
-@app.route('/top_orchestras')
-def top_orchestras():
-    orchestras = db.session.query(Tracks.orchestra, db.func.count(Tracks.id)).group_by(Tracks.orchestra).order_by(
-        db.func.count(Tracks.id).desc()).limit(10).all()
-    return render_template('analysis.html', title="Top 10 Orchestras", items=orchestras)
-
-
-@app.route('/top_conductors')
-def top_conductors():
-    conductors = db.session.query(Tracks.conductor, db.func.count(Tracks.id)).group_by(Tracks.conductor).order_by(
-        db.func.count(Tracks.id).desc()).limit(10).all()
-    return render_template('analysis.html', title="Top 10 Conductors", items=conductors)
-
-
-@app.route('/top_albums')
-def top_albums():
-    albums = db.session.query(Tracks.album, db.func.count(Tracks.id)).group_by(Tracks.album).order_by(
-        db.func.count(Tracks.id).desc()).limit(10).all()
-    return render_template('analysis.html', title="Top 10 Albums", items=albums)
-
-
-@app.route('/top_solists')
-def top_solists():
-    solists = db.session.query(Tracks.solist, db.func.count(Tracks.id)).group_by(Tracks.solist).order_by(
-        db.func.count(Tracks.id).desc()).limit(10).all()
-    return render_template('analysis.html', title="Top 10 Solists", items=solists)
-
-
-@app.route('/top_catalog_numbers')
-def top_catalog_numbers():
-    catalog_numbers = db.session.query(Tracks.catalog_number, db.func.count(Tracks.id)).group_by(
-        Tracks.catalog_number).order_by(db.func.count(Tracks.id).desc()).limit(10).all()
-    return render_template('analysis.html', title="Top 10 Catalog Numbers", items=catalog_numbers)
-
-
-@app.route('/top_eans')
-def top_eans():
-    eans = db.session.query(Tracks.ean, db.func.count(Tracks.id)).group_by(Tracks.ean).order_by(
-        db.func.count(Tracks.id).desc()).limit(10).all()
-    return render_template('analysis.html', title="Top 10 EANs", items=eans)
-
+    track_list = [{
+        'timestamp': track.timestamp,
+        'title': track.title,
+        'movement': track.movement,
+        'composer': track.composer,
+        'full_title': track.full_title,
+        'image_link': track.image_link,
+        'catalog_number': track.catalog_number,
+        'conductor': track.conductor,
+        'orchestra': track.orchestra,
+        'solist': track.solist,
+        'album': track.album,
+        'ensemble': track.ensemble,
+        'ean': track.ean,
+        'choir': track.choir
+    } for track in tracks]
+    rj = jsonify(track_list)
+    return rj
 
 if __name__ == '__main__':
     # Sicherstellen, dass das .data-Verzeichnis existiert
@@ -99,26 +62,5 @@ if __name__ == '__main__':
     # Initialisieren der Datenbank
     with app.app_context():
         db.create_all()
-
-        # Testdaten hinzufügen, wenn die Tabelle leer ist
-        if Tracks.query.count() == 0:
-            test_track = Tracks(
-                timestamp='2024-06-13 12:00',
-                title='Test Title',
-                movement='Test Movement',
-                composer='Test Composer',
-                full_title='Test Full Title',
-                image_link='http://example.com/image.jpg',
-                catalog_number='12345',
-                conductor='Test Conductor',
-                orchestra='Test Orchestra',
-                solist='Test Solist',
-                album='Test Album',
-                ensemble='Test Ensemble',
-                ean='67890',
-                choir='Test Choir'
-            )
-            db.session.add(test_track)
-            db.session.commit()
 
     app.run(debug=True)
